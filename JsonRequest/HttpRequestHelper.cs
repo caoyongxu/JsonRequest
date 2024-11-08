7455244e34350def46684c370aec29cb157e3398
Star Cao (Wicresoft) <v-ycou@microsoft.com>
2024-11-08 22:36:55 +0800

    Save First Version

---
+++ JsonRequest/HttpRequestHelper.cs
@@ -0,0 +1,41 @@
+﻿using Azure;
+using Azure.Core;
+using Azure.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonRequest
+{
+    public class HttpRequestHelper
+    {
+        public static async Task<string> ExecuteHttpPostAsync(string jsonData, string endPoint, string token = null)
+        {
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, endPoint);
+
+            if (token != null && request.Headers.Authorization == null)
+            {
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+
+            request.Content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.SendAsync(request);
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+
+        public static AccessToken GetAADPersonalToken()
+        {
+            DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential();
+            AccessToken AzOpenAIAccessToken = defaultAzureCredential.GetToken(
+                new TokenRequestContext(new string[] { "https://content-proxy-sandbox.docs.microsoft.com/.default" }));
+            return AzOpenAIAccessToken;
+        }
+
+    }
+}
---
+++ JsonRequestTest2/TestLearnGraph.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Core;
+using JsonRequest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+
+namespace JsonRequestTest2
+{
+    [TestClass]
+    public class TestLearnGraph
+    {
+        private const string Endpoint = "https://content.ops.microsoft-int.com/api/graph/query";
+        //private const string Auth_scope = "https://content-proxy-sandbox.docs.microsoft.com/.default";
+
+        [TestMethod]
+        [DataRow(Query_skills)]
+        public void TestMethod1(string query)
+        {
+            var q = query.Replace("\r\n", "").Replace(" ", "");
+            AccessToken AzOpenAIAccessToken = HttpRequestHelper.GetAADPersonalToken();
+            var result = HttpRequestHelper.ExecuteHttpPostAsync(q,
+                Endpoint, AzOpenAIAccessToken.Token).GetAwaiter().GetResult();
+            Console.WriteLine(q);
+            Console.WriteLine(result);
+
+            Assert.IsTrue(true);
+        }
+
+
+        private const string Query_skills = @"
+{
+    ""jsonData"": [
+        {
+            ""source"": {
+                ""nodeType"": ""TAXONOMY_SKILLS"",
+                ""as"": ""skills""
+            }
+        }
+    ],
+    ""select"": [
+        ""skills""
+    ]
+}
+";
+
+        private const string Query_Roles = @"";
+    }
+}
